057e6fc0d1d4eb4cc83ababe593aab7483e2e28b
sgadinga <seanandriegadingan@gmail.com>
2024-12-16 18:02:10 +0400

    Added ft_memchr.c

---
+++ Circle 0/libft/ft_memchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgadinga <sgadinga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/16 17:52:28 by sgadinga          #+#    #+#             */
+/*   Updated: 2024/12/16 18:01:57 by sgadinga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*b_s;
+
+	b_s = (unsigned char *)s;
+	while (n--)
+	{
+		if (*b_s++ == (unsigned char)c)
+			return ((void *)b_s);
+	}
+	return (NULL);
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+	
+}
