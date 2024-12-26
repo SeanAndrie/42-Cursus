e8826ddb8aad1adcf059517538a68102f0411230
sgadinga <seanandriegadingan@gmail.com>
2024-12-26 20:33:41 +0400

    Updated libft

--- Circle 0/libft/ft_split.c
+++ Circle 0/libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: sgadinga <sgadinga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 17:16:51 by sgadinga          #+#    #+#             */
-/*   Updated: 2024/12/26 12:41:02 by sgadinga         ###   ########.fr       */
+/*   Updated: 2024/12/26 20:32:47 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,4 +87,14 @@ char	**ft_split(const char *s, char c)
 		}
 	}
 	return (splits);
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+	char *str = "The quick brown fox jumped over the lazy dog";
+	char **splits = ft_split(str, ' ');
+
+	return (0);
 }
