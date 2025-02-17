093dba4d36ade3e2e47693c3676134ccbb58626f
Sean Andrie Gadingan <gseanandrie@gmail.com>
2025-02-17 10:34:27 +0400

    Migrated test files to its own folder

--- Exam Rank 02/test/test.c
+++ Exam Rank 02/test/test.c
@@ -1,22 +1,31 @@
 #include <unistd.h>
 
+int ft_tolower(int c)
+{
+    return ((c >= 'A' && c <= 'Z') ? c += 32 : c);
+}
+
+int get_alpha_idx(int c, char *alpha)
+{
+    int idx = 0;
+    int lwr_c = ft_tolower(c);
+
+    while (alpha[idx])
+    {
+        if (alpha[idx] == lwr_c)
+            return (idx + 1);
+        idx++;
+    }
+    return (-1);
+}
+
 int main(int ac, char *av[])
 {
-    size_t i;
-    size_t len;
+    char *alpha = "abcdefghijklmnopqrstuvwxyz";
 
-    if (ac == 3)
+    if (ac == 2)
     {
-        len = 0;
-        while (av[1][len]) len++;
-        i = 0;
-        while (av[1][i] && *av[2])
-        {
-            if (av[1][i] == *av[2]++)
-                i++;
-        }
-        if (i == len)
-            write(1, av[1], len);
+                
     }
     write(1, "\n", 1);
     return (0);
